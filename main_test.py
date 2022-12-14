a982ebb5680b0bd997650e95aea0e15ec0afb30a
juaki0315 <juaki0315@gmail.com>
2022-12-14 08:49:50 +0100

    setup

---
+++ main_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class TestBolos(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ makefile
@@ -0,0 +1,4 @@
+make test:
+	python3 main_test.py
+make run:
+	python3 main.py
